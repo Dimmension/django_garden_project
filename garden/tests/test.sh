5e4e0053a3d52b2f7aece72bbbbfce084d77112c
Dimmension <vadim.nikiforov.2017@gmail.com>
2024-06-17 14:04:20 +0300

    ~

--- garden/tests/test.sh
+++ garden/tests/test.sh
@@ -5,4 +5,9 @@ export PG_USER=test
 export PG_PASSWORD=test
 export PG_DBNAME=test
 
+export MINIO_USE_HTTPS=False
+export MINIO_ACCESS_KEY=Px50bigfQ2RwNKphG0Ee
+export MINIO_SECRET_KEY=H6XnWKXrsSeiRxLONkdmgoEtCs3zPIn0nphAtRmm
+export MINIO_ENDPOINT=http://localhost:9000
+
 python3 garden/manage.py test $1
